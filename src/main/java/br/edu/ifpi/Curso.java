ce0420e13d2f41dad5449d9f38b0a26b8ebf1990
alcileny <mariaalcilenycarvalho@gmail.com>
2023-11-26 16:46:34 -0300

    adicionando as classes curso e professor

--- src/main/java/br/edu/ifpi/Curso.java
+++ src/main/java/br/edu/ifpi/Curso.java
@@ -1,33 +1,413 @@
 package br.edu.ifpi;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import br.edu.ifpi.enums.StatusCurso;
 
 public class Curso {
 
-    private int idCurso;
-    private String nome;
-    private String cargaHoraria;
-    private List<Disciplina> listaDisciplinas;
-    private boolean status;
+    private Connection conexao;
 
-
-    public void adicionarDisciplina(Disciplina disciplina) {
-        this.listaDisciplinas.add(disciplina);
+    public Curso(Connection conexao) {
+        this.conexao = conexao;
     }
 
-     public void registrarNotas(Aluno aluno) {
-       
+    public void cadastrarCurso() {
+        Scanner scanner = new Scanner(System.in);
+        int papel = 0;
+        SistemaAcademico.limparConsole();
+        System.out.println("Informe o nome do curso");
+        String nome = scanner.nextLine();
+        System.out.println("Informe a carga horaria do curso");
+        String carga = scanner.nextLine();
+        System.out.println();
+        System.out.println("|-------STATUS DO CURSO------|");
+        System.out.println("| 1 - Aberto                 |");
+        System.out.println("| 2 - Fechado                |");
+        System.out.println("|----------------------------|");
+        System.out.println("Informe o status do curso");
+        papel = scanner.nextInt();
+        SistemaAcademico.limparConsole();
+        StatusCurso status = StatusCurso.ABERTO;
+
+        if (papel == 2) {
+            status = StatusCurso.FECHADO;
+        }
+
+        String query = "INSERT INTO curso (nome, carga_horaria, status) VALUES ('" + nome + "','" + carga
+                + "', '" + status + "')";
+
+        try {
+            Statement stm = this.conexao.createStatement();
+            stm.executeUpdate(query);
+            System.out.println("|------------------------------------|");
+            System.out.println("| Curso cadastrado com sucesso!      |");
+            System.out.println("|------------------------------------|");
+        } catch (SQLException e) {
+            System.out.println("|------------------------------------|");
+            System.out.println("| Erro ao realizar cadastro!         |");
+            System.out.println("|------------------------------------|");
+        }
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter para ir para o menu principal");
+        scanner2.nextLine();
     }
 
-    public void atualizarCurso(Curso curso) {
-         
+    public void atualizarCurso() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        SistemaAcademico.limparConsole();
+        int papel = 0;
+        int idCurso = carregarDadosCurso();
+        if (idCurso != 0) {
+
+            System.out.println("Informe o novo nome do curso");
+            String nome = scanner.nextLine();
+            System.out.println("Informe a nova carga horaria do curso");
+            String carga = scanner.nextLine();
+            System.out.println();
+            SistemaAcademico.limparConsole();
+            System.out.println("|-------STATUS DO CURSO------|");
+            System.out.println("| 1 - Aberto                 |");
+            System.out.println("| 2 - Fechado                |");
+            System.out.println("|----------------------------|");
+            System.out.println("Informe o status do curso");
+            papel = scanner.nextInt();
+            StatusCurso status = StatusCurso.ABERTO;
+            SistemaAcademico.limparConsole();
+            if (papel == 2) {
+                status = StatusCurso.FECHADO;
+            }
+
+            String query = "UPDATE curso SET nome = '" + nome + "', carga_horaria = '" + carga + "', status = '"
+                    + status
+                    + "' WHERE id = '" + idCurso + "'";
+
+            try {
+                Statement stm = this.conexao.createStatement();
+                stm.executeUpdate(query);
+                System.out.println("|------------------------------------|");
+                System.out.println("| Curso atualizado com sucesso!      |");
+                System.out.println("|------------------------------------|");
+            } catch (SQLException e) {
+                System.out.println("|------------------------------------|");
+                System.out.println("| Erro ao atualizar curso!           |");
+                System.out.println("|------------------------------------|");
+            }
+
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner2.nextLine();
+
+        } else {
+            System.out.println("|------------------------------------|");
+            System.out.println("| Nenhum curso encontrado!           |");
+            System.out.println("|------------------------------------|");
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner2.nextLine();
+        }
     }
 
-    public void visualizarDisciplinas() {
+    public void registrarNotas() {
+        Scanner scanner = new Scanner(System.in);
+        Aluno aluno = new Aluno(conexao);
+        Curso curso = new Curso(conexao);
+
+        SistemaAcademico.limparConsole();
+        int idAluno = aluno.carregarDadosDoAluno();
+        if (idAluno != 0) {
+            SistemaAcademico.limparConsole();
+            int idCurso = curso.carregarDadosCursoMatriculado(idAluno);
+            SistemaAcademico.limparConsole();
+            if (idCurso != 0) {
+                System.out.println("Informe a nota do aluno");
+                String nota = scanner.nextLine();
+                String query = "INSERT INTO nota (nota, aluno_id, curso_id) VALUES ('" + nota + "','" + idAluno + "','"
+                        + idCurso + "')";
+                try {
+                    Statement stm = this.conexao.createStatement();
+                    stm.executeUpdate(query);
+                    System.out.println("|--------------------------------------|");
+                    System.out.println("| Nota cadastrado com sucesso!         |");
+                    System.out.println("|--------------------------------------|");
+
+                } catch (SQLException e) {
+                    System.out.println("|--------------------------------------|");
+                    System.out.println("| Erro ao registrar nota!              |");
+                    System.out.println("|--------------------------------------|");
+                }
+
+                Scanner scanner2 = new Scanner(System.in);
+                System.out.println("Enter para ir para o menu principal");
+                scanner2.nextLine();
+
+            } else {
+                System.out.println("|--------------------------------------|");
+                System.out.println("| Nenhum curso encontrado!             |");
+                System.out.println("|--------------------------------------|");
+                Scanner scanner2 = new Scanner(System.in);
+                System.out.println("Enter para ir para o menu principal");
+                scanner2.nextLine();
+
+            }
+        } else {
+            System.out.println("|--------------------------------------|");
+            System.out.println("| Nenhum aluno encontrado!             |");
+            System.out.println("|--------------------------------------|");
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner2.nextLine();
+        }
 
     }
 
-    public void gerarEstatisticasDesempenho(Aluno aluno) {
+    public void gerarEstatisticasDesempenho() {
+        Aluno aluno = new Aluno(conexao);
+        SistemaAcademico.limparConsole();
+        int idAluno = aluno.carregarDadosDoAluno();
+        SistemaAcademico.limparConsole();
 
+        if (idAluno != 0) {
+
+            String query = "SELECT no.*, al.nome AS alnome, al.email, curs.nome AS cursos FROM aluno al LEFT JOIN nota no ON al.id = no.aluno_id LEFT JOIN curso curs ON curs.id = no.curso_id WHERE al.id = "
+                    + idAluno;
+            try {
+                Statement stm = this.conexao.createStatement();
+                ResultSet result = stm.executeQuery(query);
+
+                if (result.next()) {
+                    double nota = result.getDouble("nota");
+                    String nome = result.getString("alnome");
+                    String curso = result.getString("cursos");
+                    System.out.println("|=====ESTATISTICAS DE DESEMPENHO DO ALUNO=====|");
+                    System.out.println("| Nome: " + nome);
+                    System.out.println("|---------------------NOTAS-------------------|");
+                    if (curso == null) {
+                        System.out.println("| Nao esta matriculado em nenhum curso |");
+                    } else {
+                        System.out.println("| Curso: " + curso + " Nota: " + nota);
+                    }
+
+                    while (result.next()) {
+                        nota = result.getDouble("nota");
+                        curso = result.getString("cursos");
+                        System.out.println("| Curso: " + curso + " Nota: " + nota);
+                    }
+                    System.out.println("|=============================================|");
+
+                } else {
+                    System.out.println("|------------------------------------|");
+                    System.out.println("| Nenhum registro encontrado!        |");
+                    System.out.println("|------------------------------------|");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("|------------------------------------|");
+                System.out.println("| Erro ao carregar notas do aluno!   |");
+                System.out.println("|------------------------------------|");
+            }
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner.nextLine();
+        } else {
+            System.out.println("|------------------------------------|");
+            System.out.println("| Nenhum aluno encontrado!           |");
+            System.out.println("|------------------------------------|");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner.nextLine();
+        }
     }
+
+    public void visualizarListaDeCursos() {
+        SistemaAcademico.limparConsole();
+        String query = "SELECT * FROM curso";
+
+        try {
+            Statement stm = this.conexao.createStatement();
+            ResultSet result = stm.executeQuery(query);
+
+            if (result.next()) {
+                System.out.println("-------------LISTA DE CURSOS-----------------");
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                String carga = result.getString("carga_horaria");
+                String status = result.getString("status");
+
+                System.out.println("ID: " + id);
+                System.out.println("Nome " + nome);
+                System.out.println("Carga Horaria " + carga);
+                System.out.println("Status " + status);
+                System.out.println();
+
+                while (result.next()) {
+
+                    id = result.getInt("id");
+                    nome = result.getString("nome");
+                    carga = result.getString("carga_horaria");
+                    status = result.getString("status");
+
+                    System.out.println("ID: " + id);
+                    System.out.println("Nome " + nome);
+                    System.out.println("Carga Horaria " + carga);
+                    System.out.println("Status " + status);
+                    System.out.println();
+                }
+                System.out.println("---------------------------------------------");
+            } else {
+                System.out.println("|------------------------------------|");
+                System.out.println("| Nenhum curso encontrado!           |");
+                System.out.println("|------------------------------------|");
+            }
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner2.nextLine();
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao carregar dados dos cursos!");
+            System.out.println("----------------------------------");
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.println("Enter para ir para o menu principal");
+            scanner2.nextLine();
+        }
+    }
+
+    public int carregarDadosCurso() {
+        String query = "SELECT id, nome, status FROM curso";
+        int idSelecionado = 0;
+
+        try {
+            Statement stm = this.conexao.createStatement();
+            ResultSet result = stm.executeQuery(query);
+
+            if (result.isBeforeFirst()) {
+                System.out.println("----------------------------------");
+                while (result.next()) {
+
+                    int id = result.getInt("id");
+                    String nome = result.getString("nome");
+                    String status = result.getString("status");
+                    System.out.println(" ID -> " + id + " Nome: " + nome + " Status: " + status);
+
+                }
+                System.out.println("----------------------------------");
+
+                System.out.println("Selecione o ID do curso!");
+                Scanner scanner = new Scanner(System.in);
+                idSelecionado = scanner.nextInt();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao carregar dados do curso!");
+        }
+
+        return idSelecionado;
+    }
+
+    public int carregarDadosCursoMatriculado(int idAluno) {
+        int idSelecionado = 0;
+        String query = "SELECT cur.id, cur.nome, cur.status FROM curso cur WHERE cur.id IN (SELECT curso_id FROM curso_e_aluno WHERE aluno_id = "
+                + idAluno + ")";
+
+        try {
+            Statement stm = this.conexao.createStatement();
+            ResultSet result = stm.executeQuery(query);
+
+            if (result.isBeforeFirst()) {
+                System.out.println("----------------------------------");
+                while (result.next()) {
+
+                    int id = result.getInt("id");
+                    String nome = result.getString("nome");
+                    String status = result.getString("status");
+                    System.out.println(" ID -> " + id + " Nome: " + nome + " Status: " + status);
+                }
+                System.out.println("----------------------------------");
+
+                System.out.println("Selecione o ID do curso!");
+                Scanner scanner = new Scanner(System.in);
+                idSelecionado = scanner.nextInt();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao carregar dados do curso!");
+        }
+
+        return idSelecionado;
+    }
+
+    public int carregarDadosCursoNaoMatriculado(int idAluno) {
+        int idSelecionado = 0;
+        String query = "SELECT cur.id, cur.nome, cur.status FROM curso cur WHERE status = 'ABERTO' AND cur.id NOT IN (SELECT curso_id FROM curso_e_aluno WHERE aluno_id = "
+                + idAluno + ")";
+
+        try {
+            Statement stm = this.conexao.createStatement();
+            ResultSet result = stm.executeQuery(query);
+
+            if (result.isBeforeFirst()) {
+                System.out.println("----------------------------------");
+                while (result.next()) {
+
+                    int id = result.getInt("id");
+                    String nome = result.getString("nome");
+                    String status = result.getString("status");
+                    System.out.println(" ID -> " + id + " Nome: " + nome + " Status: " + status);
+
+                }
+                System.out.println("----------------------------------");
+
+                System.out.println("Selecione o ID do curso!");
+                Scanner scanner = new Scanner(System.in);
+                idSelecionado = scanner.nextInt();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao carregar dados do curso!");
+        }
+
+        return idSelecionado;
+    }
+
+    public int carregarDadosCursoNaoAssociado(int idProfessor) {
+        int idSelecionado = 0;
+        String query = "SELECT cur.id, cur.nome, cur.status FROM curso cur WHERE cur.id NOT IN (SELECT curso_id FROM curso_e_professor WHERE professor_id  = "
+                + idProfessor + ")";
+
+        try {
+            Statement stm = this.conexao.createStatement();
+            ResultSet result = stm.executeQuery(query);
+
+            if (result.isBeforeFirst()) {
+                System.out.println("----------------------------------");
+                while (result.next()) {
+
+                    int id = result.getInt("id");
+                    String nome = result.getString("nome");
+                    String status = result.getString("status");
+                    System.out.println(" ID -> " + id + " Nome: " + nome + " Status: " + status);
+
+                }
+                System.out.println("----------------------------------");
+
+                System.out.println("Selecione o ID do curso!");
+                Scanner scanner = new Scanner(System.in);
+                idSelecionado = scanner.nextInt();
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao carregar dados do curso!");
+        }
+
+        return idSelecionado;
+    }
+
 }
--- src/main/java/br/edu/ifpi/Professor.java
+++ src/main/java/br/edu/ifpi/Professor.java
@@ -1,22 +1,219 @@
 package br.edu.ifpi;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Professor {
 
-  private int idProfessor;
-  private String nomeProfessor;
-  private String email;
-  private List<Curso> listaCursos;
+  private Connection conexao;
 
-  public void associarCurso(Curso curso) {
+  public Professor(Connection conexao) {
+    this.conexao = conexao;
+  }
+
+  public void cadastroProfessor() {
+    Scanner scanner = new Scanner(System.in);
+    SistemaAcademico.limparConsole();
+    System.out.println("Informe o nome do professor(a)");
+    String nome = scanner.nextLine();
+    System.out.println("Informe o e-mail do professor(a)");
+    String email = scanner.nextLine();
+    SistemaAcademico.limparConsole();
+
+    String query = "INSERT INTO professor (nome, email) VALUES ('" + nome + "','" + email + "')";
+
+    try {
+      Statement stm = this.conexao.createStatement();
+      stm.executeUpdate(query);
+      System.out.println("|------------------------------------|");
+      System.out.println("| Professor cadastrado com sucesso!  |");
+      System.out.println("|------------------------------------|");
+
+      System.out.println("|---------------------------------------|");
+      System.out.println("| Vamos associar o professor a um curso!|");
+      System.out.println("|---------------------------------------|");
+      Scanner scanner2 = new Scanner(System.in);
+      System.out.println("Enter para ir para continuar");
+      scanner2.nextLine();
+
+      associarCurso();
+    } catch (SQLException e) {
+      System.out.println("|------------------------------------|");
+      System.out.println("| Erro ao realizar cadastro!         |");
+      System.out.println("|------------------------------------|");
+      Scanner scanner2 = new Scanner(System.in);
+      System.out.println("Enter para ir para o menu principal");
+      scanner2.nextLine();
+    }
+  }
+
+  public void associarCurso() {
+    Curso curso = new Curso(conexao);
+    SistemaAcademico.limparConsole();
+    int idProfessor = carregarDadosDoProfessor();
+    SistemaAcademico.limparConsole();
+    if (idProfessor != 0) {
+      int idCurso = curso.carregarDadosCursoNaoAssociado(idProfessor);
+      SistemaAcademico.limparConsole();
+      if (idCurso != 0) {
+        String query = "INSERT INTO curso_e_professor (curso_id , professor_id) VALUES ('" + idCurso + "','"
+            + idProfessor
+            + "')";
+
+        try {
+          Statement stm = this.conexao.createStatement();
+          stm.executeUpdate(query);
+          System.out.println("|------------------------------------|");
+          System.out.println("| Curso associado com sucesso!       |");
+          System.out.println("|------------------------------------|");
+        } catch (SQLException e) {
+          System.out.println("|------------------------------------|");
+          System.out.println("| Erro ao associar curso!            |");
+          System.out.println("|------------------------------------|");
+        }
+      } else {
+        System.out.println("|-------------------------------------------|");
+        System.out.println("| Nao foi encontrado nenhum curso           |");
+        System.out.println("| Que o professor nao esteja associado!     |");
+        System.out.println("|-------------------------------------------|");
+      }
+    } else {
+      System.out.println("|------------------------------------|");
+      System.out.println("| Nenhum professor encontrado!       |");
+      System.out.println("|------------------------------------|");
+    }
+
+    Scanner scanner2 = new Scanner(System.in);
+    System.out.println("Enter para ir para o menu principal");
+    scanner2.nextLine();
   }
 
   public void atualizarInformacoes() {
+    Scanner scanner = new Scanner(System.in);
+    SistemaAcademico.limparConsole();
+    int idProfessor = carregarDadosDoProfessor();
+    SistemaAcademico.limparConsole();
+    if (idProfessor != 0) {
+      System.out.println("Informe o novo nome do professor");
+      String nome = scanner.nextLine();
+      System.out.println("Informe a nova e-mail do professor");
+      String email = scanner.nextLine();
+      SistemaAcademico.limparConsole();
 
+      String query = "UPDATE professor SET nome = '" + nome + "', email = '" + email + "' WHERE id = '" + idProfessor
+          + "'";
+
+      try {
+        Statement stm = this.conexao.createStatement();
+        stm.executeUpdate(query);
+        System.out.println("|--------------------------------------------|");
+        System.out.println("| Dados do professor atualizado com sucesso! |");
+        System.out.println("|--------------------------------------------|");
+      } catch (SQLException e) {
+        System.out.println("|--------------------------------------|");
+        System.out.println("| Erro ao atualizar dados do professor!|");
+        System.out.println("|--------------------------------------|");
+      }
+    } else {
+      System.out.println("|------------------------------------|");
+      System.out.println("| Nenhum professor encontrado!       |");
+      System.out.println("|------------------------------------|");
+    }
+    Scanner scanner2 = new Scanner(System.in);
+    System.out.println("Enter para ir para o menu principal");
+    scanner2.nextLine();
   }
 
-  public void visualizarCursos() {
+  public void visualizarListaProfessores() {
+    SistemaAcademico.limparConsole();
+    String query = "SELECT assoc.*, prof.nome AS profnome, prof.email, cur.nome AS curso FROM professor prof LEFT JOIN curso_e_professor assoc ON prof.id = assoc.professor_id LEFT JOIN curso cur ON cur.id = assoc.curso_id";
 
+    try {
+      Statement stm = this.conexao.createStatement();
+      ResultSet result = stm.executeQuery(query);
+
+      if (result.next()) {
+        String nome = result.getString("profnome");
+        String email = result.getString("email");
+        String curso = result.getString("curso");
+        System.out.println("|================LISTA DE PROFESSORES=================|");
+        System.out.println("| Nome: " + nome);
+        System.out.println("| E-mail: " + email);
+        if (curso != null) {
+          System.out.println("| Curso Associado: " + curso);
+          System.out.println("|-----------------------------------------------------|");
+        } else {
+          System.out.println("| Curso Associado: Nao esta associado em nenhum curso |");
+          System.out.println("|-----------------------------------------------------|");
+        }
+        while (result.next()) {
+          nome = result.getString("profnome");
+          email = result.getString("email");
+          curso = result.getString("curso");
+          System.out.println("| Nome: " + nome);
+          System.out.println("| E-mail: " + email);
+          if (curso != null) {
+            System.out.println("| Curso Associado: " + curso);
+            System.out.println("|-----------------------------------------------------|");
+          } else {
+            System.out.println("| Curso Associado: Nao esta associado em nenhum curso |");
+            System.out.println("|-----------------------------------------------------|");
+          }
+        }
+        System.out.println("|=====================================================|");
+      }else{
+      System.out.println("|-------------------------------------------------|");
+      System.out.println("| Nenhum professor associado a cursos encontrado! |");
+      System.out.println("|-------------------------------------------------|");
+      }
+    } catch (SQLException e) {
+      System.out.println("|------------------------------------|");
+      System.out.println("| Erro ao carregar dados do aluno!   |");
+      System.out.println("|------------------------------------|");
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter para ir para o menu principal");
+    scanner.nextLine();
+  }
+
+  private int carregarDadosDoProfessor() {
+    String query = "SELECT id, nome FROM professor";
+    int idSelecionado = 0;
+
+    try {
+      Statement stm = this.conexao.createStatement();
+      ResultSet result = stm.executeQuery(query);
+
+      if (result.next()) {
+        System.out.println("----------------------------------");
+        int id = result.getInt("id");
+        String nome = result.getString("nome");
+
+        System.out.println(" ID -> " + id + " Nome: " + nome);
+
+        while (result.next()) {
+          id = result.getInt("id");
+          nome = result.getString("nome");
+          System.out.println(" ID -> " + id + " Nome: " + nome);
+        }
+        System.out.println("----------------------------------");
+        System.out.println("Selecione o ID do professor(a)!");
+        Scanner scanner = new Scanner(System.in);
+        idSelecionado = scanner.nextInt();
+      } else {
+        System.out.println("|------------------------------------|");
+        System.out.println("| Nenhum professor encontrado!           |");
+        System.out.println("|------------------------------------|");
+      }
+
+    } catch (SQLException e) {
+      System.out.println("Erro ao carregar dados do professor(a)!");
+    }
+
+    return idSelecionado;
   }
 }
